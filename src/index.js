4fb9328cdcb46b27f6abcca4de53664cd8fa8a32
Iryna Zheltukhina <irynazheltukhina@MacBook-Air.local>
2026-04-25 21:13:31 +0300

    fix-4-lab

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const userRouter = require('./user/user.router');
 const bodyParser = require('body-parser');
 const { logRequest } = require('./middleware');
 const { errorResponder } = require('./error.middleware');
+const productRouter = require('./product/product.router');
 
 const app = express();
 const PORT = 3000;
@@ -12,6 +13,7 @@ app.use(logRequest);
 app.use(bodyParser.json());
 
 app.use(productRoutes);
+app.use(productRouter);
 app.use(userRouter);
 
 app.use(errorResponder);
